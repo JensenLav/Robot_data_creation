5ce27fc59b76319f29f8c5270d8e52e66a6ec0d5
jensenlavering <@JensenLav>
2024-11-26 11:28:24 -0800

    data_create can now prompt a user for data entry of pick and place and now doesnt write over previous entries.

--- data_create.py
+++ data_create.py
@@ -1,18 +1,19 @@
 #%%
 import random
 import json
+
 #%%
 colors = ["red block", "green block", "blue block", "yellow block"]
 block_num = [1,2,3,4,5,6,7,8]
 str_table = "table"
 str_on = "on"
 #%%
-def rand_goal(array):
+def rand_goal(block_array):
     seed = random.randint(1, 1000000)
     random.seed(seed)
     rand_final = []
-    obj_num_goal = random.randint(2, len(array))
-    randomized_colors = random.sample(array, obj_num_goal)
+    obj_num_goal = random.randint(2, len(block_array))
+    randomized_colors = random.sample(block_array, obj_num_goal)
     for i in range(len(randomized_colors) - 1):
         rand_final.append((randomized_colors[i], str_on, randomized_colors[i + 1]))
     rand_final.append((randomized_colors[-1], str_on ,str_table))
@@ -21,28 +22,33 @@ def rand_goal(array):
 
 
 
-def rand_state(array, obj_num_goal):
+def rand_state(block_array, obj_num_goal):
     rand_state_arr = []
     percent_on_blocks = random.randint(1, obj_num_goal)
-    for i in range(len(array)):
+    for i in range(len(block_array)):
         if i < percent_on_blocks:
-            rand_state_arr.append((array[i], str_on, str_table))
+            rand_state_arr.append((block_array[i], str_on, str_table))
         else:
-            rand_state_arr.append((array[i], str_on, array[i-1]))
+            rand_state_arr.append((block_array[i], str_on, block_array[i-1]))
     return rand_state_arr
 #%%
-rand_goal_list = rand_goal(colors)
-print("goal: " , rand_goal_list[2])
+ def collect_data(num):
+     f = open("data_collection", "w")
+     count = 0
+     while count < num:
+        rand_goal_list = rand_goal(colors)
+        rand_state_list = rand_state(rand_goal_list[3], rand_goal_list[0])
+        combined_json = {
+            "Goal_State": rand_goal_list[2],
+            "Current_State": rand_state_list,
+            "Pick": None,
+            "Place": None
+        }
 
-rand_state_list = rand_state(rand_goal_list[3], rand_goal_list[0])
-print("current state: ", rand_state_list)
-#%%
-combined_json = {
-    "Goal_State": rand_goal_list[2],
-    "Current_State": rand_state_list,
-    "Pick": None,
-    "Place": None
-}
-
-final_json = json.dumps(combined_json, indent=3)
-print("Block Stacking:", final_json)
+        final_json = json.dumps(combined_json, indent=3)
+        f.write(f"data_{count + 1}:\n")
+        f.write(final_json)
+        f.write("\n")
+        count+=1
+     f.close()
+collect_data(1)
